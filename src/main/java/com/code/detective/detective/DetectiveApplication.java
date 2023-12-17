71629e4baca8a667c7533d5d6d28339a5aee5c3c
amarua <amarsarkar612@gmail.com>
2023-12-17 22:04:17 +0530

    program is functional

---
+++ src/main/java/com/code/detective/detective/DetectiveApplication.java
@@ -0,0 +1,13 @@
+package com.code.detective.detective;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetectiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetectiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code/detective/detective/DetectiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.detective.detective;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetectiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
